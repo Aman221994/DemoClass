8a506856df6416802707522f5c6957e4129b0f5b
Aman221994 <84703026+Aman221994@users.noreply.github.com>
2022-10-04 12:08:30 +0530

    Update index.java

--- index.java
+++ index.java
@@ -1,8 +1,8 @@
 public class hello{
-public static void main (string[] args)
+public static void main (String[] args)
 {
-for(i=1;i<=10;i++){
-  system.out.println("Hello World");
+for(int i=1;i<=10;i++){
+  System.out.println("Hello World");
 }
 }
 }
